5f75a0c3c1e617296d33d65be4274e83fd28483e
XTsat <44708609+XTsat@users.noreply.github.com>
2022-01-27 00:05:38 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -401,11 +401,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
         // TODO 添加反馈
         if (mode === MODE_ENDLESS) return '';
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '浩杰的蔑视';
+        if (score <= 99) return '吃饱喝足去rp，嘿嘿';
+        if (score <= 149) return '来两部骚一点的色图，发到群里面，我要自己看';
+        if (score <= 199) return '浩杰爱你哟';
+        return '浩杰我爱你啊';
     }
 
     w.toStr = function(obj) {
@@ -442,7 +442,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     w.initSetting = function() {
         document.getElementById("username").value = cookie("username") ? cookie("username") : "";
         document.getElementById("message").value = cookie("message") ? cookie("message") : "";
-        document.getElementsByTagName("title")[0].innerText = cookie("title") ? cookie("title") : "吃掉小鹿乃";
+        document.getElementsByTagName("title")[0].innerText = cookie("title") ? cookie("title") : "吃掉💊";
         if (cookie("keyboard")) {
             document.getElementById("keyboard").value = cookie("keyboard");
             map = {}
